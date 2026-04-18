8d91d75dbf24dc00a98db353d2af85d07efd5dde
Anthony Harr <anthony.harr4@gmail.com>
2026-04-18 18:20:13 -0400

    force google meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,5 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "CineFusion",
-  description: "Create without permission.",
-  verification: {
-    google: "t5RQlgmckYO_0hHx39WGzFkd-19067qrxy7eL",
-  },
-};
-
 export default function RootLayout({
   children,
 }: {
@@ -15,9 +7,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-[#070712] text-white">
-        <div className="min-h-screen">{children}</div>
-      </body>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="t5RQlgmckYO_0hHx39WGzFkd-19067qrxy7eL"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
